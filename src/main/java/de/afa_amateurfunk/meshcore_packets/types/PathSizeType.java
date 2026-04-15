591d01033dd78d40a4f1bf499262c0f7bc8f42e9
Marco Schuster <m.schuster91@googlemail.com>
2026-04-15 10:47:52 +0200

    PathSizeType: improve documentation

--- src/main/java/de/afa_amateurfunk/meshcore_packets/types/PathSizeType.java
+++ src/main/java/de/afa_amateurfunk/meshcore_packets/types/PathSizeType.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.NoSuchElementException;
 import java.util.stream.Stream;
 
 /**
@@ -13,9 +14,17 @@ import java.util.stream.Stream;
  * @link <a href="https://github.com/meshcore-dev/MeshCore/blob/main/docs/packet_format.md">upstream doc</a>
  */
 public enum PathSizeType {
-
+    /**
+     * 1 byte per hop
+     */
     SIZE_1(0x00, "PATH_1_BYTE_PER_HOP", 1),
+    /**
+     * 2 bytes per hop
+     */
     SIZE_2(0x01, "PATH_2_BYTES_PER_HOP", 2),
+    /**
+     * 3 bytes per hop
+     */
     SIZE_3(0x02, "PATH_3_BYTES_PER_HOP", 3);
 
     /*
@@ -23,11 +32,31 @@ public enum PathSizeType {
 
     @link https://github.com/meshcore-dev/MeshCore/blob/dev/src/Dispatcher.cpp#L167
      */
+    /**
+     * logger
+     */
     private static final Logger LOG = LoggerFactory.getLogger(PathSizeType.class);
+    /**
+     * bitmask representing this enum's value
+     */
     private final int bitmask;
+    /**
+     * upstream specification name for this enum
+     */
     private final String specName;
+    /**
+     * creature comfort to access the calculation-relevant value (so that we don't have to add +1 to the bitmask like upstream does)
+     * <p>note: do not assume that, should upstream implement 0x03, it will represent 4 bytes per hop</p>
+     */
     private final int bytesPerHop;
 
+    /**
+     * construct a new enum member
+     *
+     * @param bitmask     input bitmask
+     * @param specName    input spec-name
+     * @param bytesPerHop input bytes per hop
+     */
     PathSizeType(int bitmask, String specName, int bytesPerHop) {
         this.bitmask = bitmask;
         this.specName = specName;
@@ -39,9 +68,10 @@ public enum PathSizeType {
      *
      * @param rawByte header byte
      * @return PathSizeType corresponding to the packet's specification
+     * @throws java.util.NoSuchElementException when an invalid header byte is encountered
      * @link <a href="https://github.com/meshcore-dev/MeshCore/blob/main/src/Packet.h#L79">upstream code</a>
      */
-    public static PathSizeType fromHeader(byte rawByte) {
+    public static PathSizeType fromHeader(byte rawByte) throws NoSuchElementException {
         LOG.trace(String.format("Determining path size from %02x / %s", rawByte, StringUtils.leftPad(Integer.toBinaryString(rawByte & 0xFF), 8, '0')));
         //LOG.trace(String.format("%s",StringUtils.leftPad(Integer.toBinaryString(((rawByte & 0xFF) >> 6) & 0xFF),8,'0')));
         PathSizeType ret = Stream.of(PathSizeType.values()).filter(el -> (((rawByte & 0xFF) >> 6) & 0xFF) == el.bitmask).limit(1).toList().getFirst();
@@ -49,19 +79,39 @@ public enum PathSizeType {
         return ret;
     }
 
+    /**
+     * pretty-print when someone tries to dump an instance into a string, we fall back to the specification name for recognizability
+     *
+     * @return spec name
+     */
     @Override
     public String toString() {
         return specName;
     }
 
+    /**
+     * get bitmask field
+     *
+     * @return bitmask
+     */
     public int getBitmask() {
         return bitmask;
     }
 
+    /**
+     * get specName field
+     *
+     * @return upstream name
+     */
     public String getSpecName() {
         return specName;
     }
 
+    /**
+     * get bytesPerHop field
+     *
+     * @return bytes per hop
+     */
     public int getBytesPerHop() {
         return bytesPerHop;
     }
